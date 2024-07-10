94e193f9132ffd214475158b79c24e9c9e28e579
Dalton Stevens <d.72.stevens@gmail.com>
2024-07-09 20:53:17 -0500

    code wars challenge

---
+++ twoDecimalPlaces.js
@@ -0,0 +1,7 @@
+const twoDecimalPlaces = (n) => {
+  return +n.toFixed(2);
+};
+
+console.log(twoDecimalPlaces(4.659725356));
+console.log(twoDecimalPlaces(173735326.3783732637948948));
+console.log(twoDecimalPlaces(4.653725356));
