92d9d3d226d9b799baa8b411cce048ce7c83582a
Dalton Stevens <d.72.stevens@gmail.com>
2024-08-12 17:01:31 -0500

    code wars challenge

---
+++ templateStrings.js
@@ -0,0 +1,7 @@
+const templateStrings = (obj, feature) => {
+  return `${obj} are ${feature}`;
+};
+
+console.log(templateStrings("Animals", "Good"));
+console.log(templateStrings("You", "special"));
+console.log(templateStrings("lives", "frozen"));
