64eee41659b08e6da32661e392186077a076f127
Dalton Stevens <d.72.stevens@gmail.com>
2024-08-18 19:35:15 -0500

    code wars challenge

---
+++ apple.js
@@ -0,0 +1,8 @@
+const apple = (x) => {
+  return x * x > 1000
+    ? `It's hotter than the sun!!`
+    : `Help yourself to a honeycomb Yorkie for the glovebox.`;
+};
+
+console.log(apple("50"));
+console.log(apple(4));
